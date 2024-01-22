9dba43b1dff6a12e67e32e41ed16611af9fb5be2
lvhl2 <lvhl2@outlook.com>
2024-01-23 02:16:44 +0800

    build.sh 脚本提交-提升权限

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "运行容器python执行自动化"  #输出日志
 #-w=$WORKSPACE：指定workspace
 #--volumes-from=jenkins_save01：将jenkins容器中的workspace映射到python容器中，此时jenkins中git拉下来的代码就会被映射进去
-docker run --rm -w=$WORKSPACE --volumes-from=jenkins lv_python_test
+sudo docker run --rm -w=$WORKSPACE --volumes-from=jenkins lv_python_test
 echo "python执行自动化执行成功"
 
 
